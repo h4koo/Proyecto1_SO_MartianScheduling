97c8ae1f27f6523c304d299353e82e1ec428d37e
Jung <bakkim05@gmail.com>
2020-11-02 21:05:09 -0600

    bug fix scheduler

--- visual/VisualGUI.py
+++ visual/VisualGUI.py
@@ -18,6 +18,7 @@ class VisualGUI:
     def loadData(self):
         processArray = []
         frecuencyArray = []
+        singleArray = []
         total = 0
         elements = 0
 
@@ -25,46 +26,63 @@ class VisualGUI:
         lines = scheduleFile.readlines()
 
         for line in lines:
+            if line.rstrip("\n") not in processArray:
+                singleArray.append(line.rstrip("\n"))
+                elements+=1
+
+
             if not processArray:
                 frecuencyArray.append(1)
                 processArray.append(line.rstrip("\n"))
                 total = 1
-                elements+=1
 
             elif (processArray[-1] != line.rstrip("\n")):
                 frecuencyArray.append(1)
                 processArray.append(line.rstrip("\n"))
                 total += 1
-                if (processArray.index(line.rstrip("\n")) == 0):
-                    elements+=1
 
             else:
                 frecuencyArray[-1] += 1
                 total += 1
 
-        self.drawData(self.canvasWidth//total,self.canvasHeight//elements, processArray,frecuencyArray)
+        print(processArray)
+        # print(frecuencyArray)
+        # print(total)
+        # print(elements)
+
+        self.drawData(self.canvasWidth//total,self.canvasHeight//elements, processArray,frecuencyArray,total,elements,singleArray)
 
 
-    def drawData(self, boxWidth, boxHeight, processArray, frecuencyArray):
+    def drawData(self, boxWidth, boxHeight, processArray, frecuencyArray,total,elements,singleArray):
+        for i in range(total):
+            for j in range(elements):
+                self.canvas.create_rectangle(i*boxWidth,j*boxHeight,i*boxWidth+boxWidth,j*boxHeight+boxHeight)
+
         acum = 0
         for i in range(len(processArray)):
             frec = frecuencyArray[i]
-            y = processArray.index(processArray[i])
+            y = singleArray.index(processArray[i])
+            print(y)
             color = self.colorPicker(y)
 
             for j in range(frec):
-                self.canvas.create_rectangle(acum*boxWidth,boxHeight*y,acum*boxWidth + boxWidth,y*boxHeight + boxHeight, fill = color)
+                self.canvas.create_rectangle(
+                    acum*boxWidth,
+                    boxHeight*y,
+                    acum*boxWidth + boxWidth,
+                    y*boxHeight + boxHeight, 
+                    fill = color)
                 acum += 1
 
         acum = 0
         for i in range(len(processArray)):
             frec = frecuencyArray[i]
-            y = processArray.index(processArray[i])
+            y = singleArray.index(processArray[i])
 
             x_text = ((acum*boxWidth) + ((((acum + frec) * boxWidth) - (acum*boxWidth))/2))
             y_text = ((boxHeight* y) + ((((boxHeight* y)+boxHeight) - (boxHeight* y))/2))
 
-            self.canvas.create_text(x_text,y_text,fill="black",font="Helvetica 20", text = processArray[i])
+            self.canvas.create_text(x_text,y_text,fill="black",font="Helvetica 16", text = processArray[i])
 
             acum +=frec
 
