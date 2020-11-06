ba64b84b40483a88def8ba215741df59a6b2cc4b
Jung <bakkim05@gmail.com>
2020-11-05 23:21:01 -0600

    last change

--- visual/main.py
+++ visual/main.py
@@ -2,6 +2,11 @@ from VisualGUI import VisualGUI
 from tkinter import Tk
 
 if __name__ == "__main__":
-    root = Tk()
-    gui = VisualGUI(root)
-    root.mainloop()
+    rm_root = Tk()
+    eds_root = Tk()
+
+    rm_gui = VisualGUI(rm_root,"Simulacion RM","rm_report.txt")
+    eds_gui = VisualGUI(eds_root,"Simulacion EDS","eds_report.txt")
+
+    rm_root.mainloop()
+    eds_root.mainloop()
